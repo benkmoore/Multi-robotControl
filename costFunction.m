fd2ebbb3f42f3846abeda25b015e52abd69d0c46
benkmoore <ben.karl.moore@gmail.com>
2020-03-02 16:53:37 -0800

    norm of states and input in cost fct and grid search for rel. weighting

--- costFunction.m
+++ costFunction.m
@@ -1,9 +1,24 @@
 function J = costFunction(z, mpc)
 [X, U] = getDecisions(z, mpc);
 X = X'; U = U';
-cost = sum(diag((X(:,1:end-1)-mpc.x_d(:,mpc.current:mpc.current + mpc.predictionHorizon-2))'*mpc.Q ...
-    *(X(:,1:end-1)-mpc.x_d(:,mpc.current:mpc.current + mpc.predictionHorizon-2)))) ...
-      + sum(diag(U'*mpc.P*U));
+
+% Normalize state and input
+x_bar = (X(:,1:end-1)-mpc.x_d(:,mpc.current:mpc.current + mpc.predictionHorizon-2));
+norm_x_bar = norm(x_bar);
+if norm_x_bar > 0
+    x_bar = x_bar/norm_x_bar;
+end
+
+u_bar = U;
+norm_U = norm(U);
+if norm_U > 0
+    u_bar = u_bar/norm_U;
+end
+
+cost = sum(diag(x_bar'*mpc.Q*x_bar)) ...
+      + sum(diag(u_bar'*mpc.P*u_bar));
+  
 J = cost + ...
-    (X(:,end) - mpc.x_d(:,mpc.current + mpc.predictionHorizon - 1))' * mpc.Qn * (X(:,end) - mpc.x_d(:,mpc.current + mpc.predictionHorizon - 1));
+    ((X(:,end) - mpc.x_d(:,mpc.current + mpc.predictionHorizon - 1))/norm(x_bar))' * mpc.Qn * ...
+        ((X(:,end) - mpc.x_d(:,mpc.current + mpc.predictionHorizon - 1))/norm(x_bar));
 end
--- setupMPC.m
+++ setupMPC.m
@@ -1,70 +1,104 @@
-N = 4;
-nx = 6 * N;
-nu = 3 * N;
+close all
+clear all
 
-mpc.N = N;
-mpc.nx = nx;
-mpc.nu = nu;
-mpc.duration = 50;
-mpc.controlHorizon = 2;
-mpc.predictionHorizon = 5;
-mpc.dt = 0.01;
-mpc.totalDuration = mpc.duration * mpc.dt;
+% weight of u vs x
+pows = -20 : -15;
+rel_weight = 10.^pows;
+errors = [];
 
-[mpc.x_d, x_r, y_r] = getDesiredReference(mpc,0);
-A = [1 mpc.dt;
-    0 1];
-B = [mpc.dt^2/2;
-    mpc.dt];
+for i = 1 : length(rel_weight)
+    N = 4;
+    nx = 6 * N;
+    nu = 3 * N;
 
-A_kron = kron(kron(A, eye(3, 3)), eye(N, N));
-B_kron = kron(kron(B, eye(3, 3)), eye(N, N));
-mpc.stateFct = @(x,u) A_kron * x + B_kron * u;
-mpc.costFct = @costFunction;
-mpc.xMin = [-Inf; -Inf; -Inf; -15; -15; -15] * Inf;
-mpc.xMax = [Inf; Inf; Inf; 15; 15; 15] * Inf;
-mpc.uMin = [-4; -4; -4] * Inf;
-mpc.uMax = -mpc.uMin;
-mpc.Q = kron(diag([10000, 10000, 10000, 5, 5, 5]), eye(N, N))*100;
-mpc.P = kron(eye(3,3), eye(N, N))*0.001;
-mpc.Qn = kron(diag([50, 50, 50, 50, 50, 50]), eye(N ,N))*100;
-mpc.x0 = mpc.x_d(:,1);%zeros(mpc.nx, 1);
-mpc.u0 = zeros(mpc.nu, 1);
-mpc.current = 1;
+    mpc.rho = rel_weight(i);
+    mpc.N = N;
+    mpc.nx = nx;
+    mpc.nu = nu;
+    mpc.duration = 50;
+    mpc.controlHorizon = 2;
+    mpc.predictionHorizon = 5;
+    mpc.dt = 0.01;
+    mpc.totalDuration = mpc.duration * mpc.dt;
 
-mpc.boundX_ub = repmat([Inf,Inf,Inf,10,10,10]', mpc.N, 1);
-mpc.boundU_ub = repmat([5,5,5]', mpc.N, 1);
-mpc.boundX_lb = - mpc.boundX_ub;
-mpc.boundU_lb = - mpc.boundU_ub;
-[z2uMap, u2zMap] = mapControls(mpc);
-mpc.z2uMap = z2uMap; mpc.u2zMap = u2zMap;  
+    [mpc.x_d, x_r, y_r] = getDesiredReference(mpc,0);
+    A = [1 mpc.dt;
+        0 1];
+    B = [mpc.dt^2/2;
+        mpc.dt];
 
-[Aeq, Beq] = setEqConstraints(mpc, A_kron, B_kron);
-mpc.A = Aeq; mpc.B = Beq;
+    A_kron = kron(kron(A, eye(3, 3)), eye(N, N));
+    B_kron = kron(kron(B, eye(3, 3)), eye(N, N));
+    mpc.stateFct = @(x,u) A_kron * x + B_kron * u;
+    mpc.costFct = @costFunction;
+    mpc.xMin = [-Inf; -Inf; -Inf; -15; -15; -15] * Inf;
+    mpc.xMax = [Inf; Inf; Inf; 15; 15; 15] * Inf;
+    mpc.uMin = [-4; -4; -4] * Inf;
+    mpc.uMax = -mpc.uMin;
 
-x = mpc.x0; u = mpc.u0;
-Xdef = []; Udef = [];
-for t = 1:mpc.duration - mpc.predictionHorizon
-    fprintf('%g \n',t)
-    z = mpcDecision(mpc, x, u);
-    [X, U] = getDecisions(z, mpc);
-    x = X(1,:)';
-    u = U(1,:)';
-    Xdef = [Xdef; x'];
-    Udef = [Udef; u'];
-    mpc.current = mpc.current + 1;
+    mpc.x0 = mpc.x_d(:,1);%zeros(mpc.nx, 1);
+    mpc.u0 = zeros(mpc.nu, 1);
+    mpc.current = 1;
+
+    mpc.boundX_ub = repmat([Inf,Inf,Inf,10,10,10]', mpc.N, 1);
+    mpc.boundU_ub = repmat([5,5,5]', mpc.N, 1);
+    mpc.boundX_lb = - mpc.boundX_ub;
+    mpc.boundU_lb = - mpc.boundU_ub;
+    
+    % Cost on states
+    mpc.Q = kron(diag([1, 1, 1, 0, 0, 0]), eye(N, N));
+    %mpc.Q = A_kron'*A_kron;
+    mpc.Qn = kron(diag([10^10, 10^10, 10^10, 10^10, 10^10, 10^10]), eye(N ,N));
+ 
+    % Cost on input
+    mpc.P = kron(eye(3,3), eye(N, N))*mpc.rho;
+
+    [z2uMap, u2zMap] = mapControls(mpc);
+    mpc.z2uMap = z2uMap; mpc.u2zMap = u2zMap;  
+
+    [Aeq, Beq] = setEqConstraints(mpc, A_kron, B_kron);
+    mpc.A = Aeq; mpc.B = Beq;
+
+    x = mpc.x0; u = mpc.u0;
+    Xdef = []; Udef = [];
+    fprintf('iter: %g \n',i)
+    for t = 1:mpc.duration - mpc.predictionHorizon
+        fprintf('%g \n',t)
+        z = mpcDecision(mpc, x, u);
+        [X, U] = getDecisions(z, mpc);
+        x = X(1,:)';
+        u = U(1,:)';
+        Xdef = [Xdef; x'];
+        Udef = [Udef; u'];
+        mpc.current = mpc.current + 1;
+    end
+    err = error(mpc, Xdef);
+    errors = [errors, err];
+    
+    figure();
+    hold on;
+    for t = 1:mpc.duration - mpc.predictionHorizon
+        plot(mpc.x_d(1,t), mpc.x_d(2,t),'-o','MarkerSize',5,'MarkerFaceColor','r')
+        plot(mpc.x_d(7,t), mpc.x_d(8,t),'-o','MarkerSize',5,'MarkerFaceColor','g')
+        plot(Xdef(t,1),Xdef(t,2),'-o','MarkerSize',2.5,'MarkerFaceColor','k');
+        plot(Xdef(t,7),Xdef(t,8),'-o','MarkerSize',2.5,'MarkerFaceColor','b');
+    end
 end
 
+figure()
+hold on
+plot(rel_weight, errors)
+xlabel('rel. weight')
+ylabel('Error')
+grid on
 
 
-figure();
-for t = 1:mpc.duration - mpc.predictionHorizon
-    plot(mpc.x_d(1,t), mpc.x_d(2,t),'-or','MarkerSize',5,'MarkerFaceColor','r')
-    hold on;
-    plot(Xdef(t,1),Xdef(t,2),'-or','MarkerSize',5,'MarkerFaceColor','k');
-%     plot(Xdef(t,7),Xdef(t,8),'or','MarkerSize',5,'MarkerFaceColor','b');
-%     plot(Xdef(t,13),Xdef(t,14),'or','MarkerSize',5,'MarkerFaceColor','g');
-%     plot(Xdef(t,19),Xdef(t,20),'or','MarkerSize',5,'MarkerFaceColor','y');
-%     pause(0.4)
+% errors
+function tot_e = error(mpc, Xdef)
+    e_tr1 = norm(mpc.x_d(1:2,1:mpc.duration - mpc.predictionHorizon) - Xdef(:,1:2)');
+    e_tr2 = norm(mpc.x_d(7:8,1:mpc.duration - mpc.predictionHorizon) - Xdef(:,7:8)');
+    e_tr3 = norm(mpc.x_d(13:14,1:mpc.duration - mpc.predictionHorizon) - Xdef(:,13:14)');
+    e_tr4 = norm(mpc.x_d(19:20,1:mpc.duration - mpc.predictionHorizon) - Xdef(:,19:20)');
+    tot_e = e_tr1 + e_tr2 + e_tr3 + e_tr4;
 end
 
