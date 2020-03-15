760c8397ffa410be79abb9160752d468144e35f1
wvangijseghem <woutervangijseghem@gmail.com>
2020-03-15 12:14:57 -0700

    Changes for presentation

--- getDesiredReference.m
+++ getDesiredReference.m
@@ -1,4 +1,4 @@
-function [x_d, x_r, y_r] = getDesiredReference(mpc, boolPlot, nTraj)
+function [x_d, x_r, y_r, z_r] = getDesiredReference(mpc, boolPlot, nTraj, mD)
     x_d = zeros(mpc.nx, mpc.paddedDuration);
     switch nTraj
         case 1
@@ -10,8 +10,9 @@ function [x_d, x_r, y_r] = getDesiredReference(mpc, boolPlot, nTraj)
             cs = spline(x_r,[0 y_r 0]); 
             x_r = linspace(0, 8, mpc.duration);
             y_r = ppval(cs, x_r);
+            z_r = [linspace(0,8,mpc.duration)];
             y_prime = ppval(fnder(cs,1),x_r);
-            prime = [ones(size(y_prime)); y_prime; zeros(size(y_prime))];
+            prime = [ones(size(y_prime)); y_prime; ones(size(y_prime))];
             x_d = zeros(mpc.nx, mpc.duration);
             for t = 1:mpc.duration
                 if prime(1,t) == 0
@@ -25,16 +26,18 @@ function [x_d, x_r, y_r] = getDesiredReference(mpc, boolPlot, nTraj)
                     gamma = atan2(prime(3,t),prime(2,t));   
                 end
                 R = roty(rad2deg(-gamma)) * rotz(rad2deg(-psi));
-                x_d(:,t) = reshape([[x_r(t);y_r(t);0] + R * x_dr(:,1:mpc.N); repmat(prime(:,t),1,mpc.N)],mpc.nx,1);
+                x_d(:,t) = reshape([[x_r(t);y_r(t);z_r] + R * x_dr(:,1:mpc.N); repmat(prime(:,t),1,mpc.N)],mpc.nx,1);
             end
         otherwise
             x_dr = [-1/sqrt(2), 1/sqrt(2), 1/sqrt(2), -1/sqrt(2);
                 1/sqrt(2), 1/sqrt(2), -1/sqrt(2), -1/sqrt(2);
                 0, 0, 0 ,0];
+            x_dr = repmat(x_dr,1,10);
             x_r = linspace(0, 8, round(0.5 * mpc.duration));
             x_r = [x_r, 8 * ones(1, mpc.duration - round(0.5 * mpc.duration))];
             y_r = zeros(1, round(0.5 * mpc.duration));
             y_r = [y_r, linspace(0, 8, mpc.duration - round(0.5 * mpc.duration))];
+            z_r = [0 * linspace(0,8,mpc.duration)];
             prime = [ones(1, round(0.5 * mpc.duration)), zeros(1, mpc.duration - round(0.5 * mpc.duration));
                 zeros(1, round(0.5 * mpc.duration)), ones(1, mpc.duration - round(0.5 * mpc.duration));
                 zeros(1, mpc.duration)];
@@ -50,26 +53,45 @@ function [x_d, x_r, y_r] = getDesiredReference(mpc, boolPlot, nTraj)
                     gamma = atan2(prime(3,t),prime(2,t));   
                 end
                 R = roty(rad2deg(-gamma)) * rotz(rad2deg(-psi));
-                x_d(:,t) = reshape([[x_r(t);y_r(t);0] + R * x_dr(:,1:mpc.N); repmat(prime(:,t),1,mpc.N)],mpc.nx,1);
+                x_d(:,t) = reshape([[x_r(t);y_r(t);z_r(t)] + R * x_dr(:,1:mpc.N); repmat(prime(:,t),1,mpc.N)],mpc.nx,1);
             end
     end
     x_d(:, mpc.duration + 1: mpc.paddedDuration) = repmat(x_d(:,mpc.duration),1,mpc.predictionHorizon); 
     if (boolPlot)
-        figure();
-        for t = 1:mpc.duration
-            plot(x_r(t), y_r(t),'-or','MarkerSize',5,'MarkerFaceColor','r')
-            hold on;
-            plot(x_d(1,t),x_d(2,t),'-or','MarkerSize',5,'MarkerFaceColor','k');
-            if mpc.N > 1
-                plot(x_d(7,t),x_d(8,t),'or','MarkerSize',5,'MarkerFaceColor','b');
+        if ~mD
+            figure();
+            for t = 1:mpc.duration
+                plot(x_r(t), y_r(t),'-or','MarkerSize',5,'MarkerFaceColor','r')
+                hold on;
+                plot(x_d(1,t),x_d(2,t),'-or','MarkerSize',5,'MarkerFaceColor','k');
+                if mpc.N > 1
+                    plot(x_d(7,t),x_d(8,t),'or','MarkerSize',5,'MarkerFaceColor','b');
+                end
+                if mpc.N > 2
+                    plot(x_d(13,t),x_d(14,t),'or','MarkerSize',5,'MarkerFaceColor','g');
+                end
+                if mpc.N > 3
+                    plot(x_d(19,t),x_d(20,t),'or','MarkerSize',5,'MarkerFaceColor','y');
+                end
+                pause(0.4)
             end
-            if mpc.N > 2
-                plot(x_d(13,t),x_d(14,t),'or','MarkerSize',5,'MarkerFaceColor','g');
+        else
+            figure();
+            for t = 1:mpc.duration
+                plot3(x_r(t), y_r(t),z_r(t),'-or','MarkerSize',5,'MarkerFaceColor','r')
+                hold on;
+                plot3(x_d(1,t),x_d(2,t),x_d(3,t),'-or','MarkerSize',5,'MarkerFaceColor','k');
+                if mpc.N > 1
+                    plot3(x_d(7,t),x_d(8,t),x_d(9,t),'or','MarkerSize',5,'MarkerFaceColor','b');
+                end
+                if mpc.N > 2
+                    plot3(x_d(13,t),x_d(14,t),x_d(15,t),'or','MarkerSize',5,'MarkerFaceColor','g');
+                end
+                if mpc.N > 3
+                    plot3(x_d(19,t),x_d(20,t),x_d(21,t),'or','MarkerSize',5,'MarkerFaceColor','y');
+                end
+                pause(0.4)
             end
-            if mpc.N > 3
-                plot(x_d(19,t),x_d(20,t),'or','MarkerSize',5,'MarkerFaceColor','y');
-            end
-            pause(0.4)
         end
     end
 end
--- setObstacles.m
+++ setObstacles.m
@@ -1,4 +1,4 @@
-function obstacles = setObstacles()
+function obstacles = setObstacles(c)
     x_obs1 = 4;
     y_obs1 = 0;
     z_obs1 = 0;
@@ -9,8 +9,8 @@ function obstacles = setObstacles()
     r_obs2 = 0.3;
     
     obstacles = [[x_obs1, y_obs1, z_obs1, r_obs1]',[x_obs2, y_obs2, z_obs2, r_obs2]'];
-    obstacles = [2 + (10-2) * rand(1,10);
-                3 * rand(1,10);
-                 zeros(1,10);
-                 0.5 * rand(1,10)];
+    obstacles = [2 + (10-2) * rand(1,c);
+                4 * rand(1,c);
+                 zeros(1,c);
+                 0.5 * rand(1,c)];
 end
